471f1c749433ce28f806b83f8a34ea9ff6140800
Alex <alexandru.indries@emanuel.ro>
2020-11-26 19:24:43 +0200

    Done!

--- index.js
+++ index.js
@@ -1,43 +1,89 @@
 // Get all Projects
-async function getProjects(){
+async function getProjects() {
     var projects;
     await fetch("https://app.paymoapp.com/api/projects/", {
-        headers: {
-            "X-Session": "5059fe5ba060edfd2e29cf241a40d1fd"
-        },
-    })
-    .then(res => res.json())
-    .then(data => {
-    projects = data;
-    console.log(data)
-})
+            headers: {
+                "X-Session": "5059fe5ba060edfd2e29cf241a40d1fd"
+            },
+        })
+        .then(res => res.json())
+        .then(data => {
+            projects = data;
+        })
+    return projects;
 }
-getProjects();
 // Get all Tasklists
-async function getTasklists(){
+async function getTasklists() {
     var tasklists;
-await fetch("https://app.paymoapp.com/api/tasklists/", {
-        headers: {
-            "X-Session": "5059fe5ba060edfd2e29cf241a40d1fd"
-        },
-    })
-    .then(res => res.json())
-    .then(data => {
-    tasklists = data;
-    console.log(data)})
+    await fetch("https://app.paymoapp.com/api/tasklists/", {
+            headers: {
+                "X-Session": "5059fe5ba060edfd2e29cf241a40d1fd"
+            },
+        })
+        .then(res => res.json())
+        .then(data => {
+            tasklists = data;
+        })
+    return tasklists;
 }
-getTasklists();
 // Get all Tasks
-async function getTasks(){
+async function getTasks() {
     var tasks;
-await fetch("https://app.paymoapp.com/api/tasks/", {
-        headers: {
-            "X-Session": "5059fe5ba060edfd2e29cf241a40d1fd"
-        },
-    })
-    .then(res => res.json())
-    .then(data => {
-    tasks = data;
-    console.log(data)})
+    await fetch("https://app.paymoapp.com/api/tasks/", {
+            headers: {
+                "X-Session": "5059fe5ba060edfd2e29cf241a40d1fd"
+            },
+        })
+        .then(res => res.json())
+        .then(data => {
+            tasks = data;
+        })
+    return tasks;
 }
-getTasks();
+
+async function createList() {
+    let projects;
+    let tasklists;
+    let tasks;
+
+    await getProjects().then((res) => {
+        projects = res.projects;
+    })
+
+    await getTasklists().then((res) => {
+        tasklists = res.tasklists;
+    })
+
+    await getTasks().then((res) => {
+        tasks = res.tasks;
+    })
+
+
+    var list = {};
+
+    projects.forEach(element => {
+        element.tasklists = {};
+        let subList = {
+            name: element.name,
+            tasklists: {}
+        }
+        list[element.id] = subList;
+    });
+
+    tasklists.forEach(element => {
+        element.tasks = {};
+        let subList = {
+            name: element.name,
+            tasks: {}
+        }
+        list[element.project_id].tasklists[element.id] = subList;
+    });
+
+    tasks.forEach(element => {
+        list[element.project_id].tasklists[element.tasklist_id].tasks[element.id] = element.name;
+    });
+
+    console.log(list);
+}
+
+createList();
